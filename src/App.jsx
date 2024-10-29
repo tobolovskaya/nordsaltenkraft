6e5e3d4b3bea4e7e7f61c0c07c4dd51cd1c9cd73
tobolovskaya <142045878+tobolovskaya@users.noreply.github.com>
2024-10-29 23:32:10 +0100

    1

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header/Header';
 import StrømApp from './components/StrømApp/StrømApp';
 import Strømavtalen from './components/Strømavtalen/Strømavtalen'
 import Bredbånd from './components/Bredbånd/Bredbånd'
+import Kraftproduksjon from './components/Kraftproduksjon/Kraftproduksjon'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <StrømApp />
       <Strømavtalen />
       <Bredbånd />
+      <Kraftproduksjon />
       
     </div>
   );
---
+++ src/components/Kraftproduksjon/Kraftproduksjon.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styles from './Kraftproduksjon.module.css';
+
+const Kraftproduksjon = () => {
+  return (
+    <section id="kraftproduksjon" className={styles.aboutSection}>
+      <div className="container">
+              <h2 className={styles.sectiontitle}>Kraftproduksjon</h2>
+              <p className={styles.sectionsubtitle}>
+          Nord-Salten Kraft har en sterk lokal forankring i Nord-Salten, med lokal produksjon og kompetanse, og lokalt hovedkontor. Vi er stolte av å produsere fornybar energi, og å spille en viktig samfunnsrolle i regionen.
+        </p>
+        <div className={styles.aboutHolder}>
+          <ul className={styles.aboutList}>
+            <li className={styles.aboutItem}>
+              <h3 className={styles.aboutText}>Slunkajavrre kraftverk</h3>
+              Ligger i Hamarøy kommune.<br />
+              Anlegget ble satt i drift i 1983.
+            </li>
+            <li className={styles.aboutItem}>
+              <h3 className={styles.aboutText}>Rekvatn kraftverk</h3>
+              Ligger i Hamarøy kommune.<br />
+              Anlegget ble satt i drift i 1953.
+            </li>
+            <li className={styles.aboutItem}>
+              <h3 className={styles.aboutText}>Sagfossen kraftverk</h3>
+              Ligger i Hamarøy kommune.<br />
+              Anlegget ble satt i drift i 1979.
+            </li>
+            <li className={styles.aboutItem}>
+              <h3 className={styles.aboutText}>Forsanvatn kraftverk</h3>
+              Ligger i Steigen kommune.<br />
+              Anlegget ble satt i drift i 2013.
+            </li>
+            <li className={styles.aboutItem}>
+              <h3 className={styles.aboutText}>Forsanvatn minikraftverk</h3>
+              Ligger i Steigen kommune.<br />
+              Anlegget ble satt i drift i 2013.
+            </li>
+            <li className={styles.aboutItem}>
+              <h3 className={styles.aboutText}>Raukforsen kraftverk</h3>
+              Ligger i Sørfold kommune.<br />
+              Anlegget ble satt i drift i 2021.
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Kraftproduksjon;
