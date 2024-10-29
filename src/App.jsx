a463a58a1e8c8ec3baec165e25d1f8bcfffc2de1
tobolovskaya <142045878+tobolovskaya@users.noreply.github.com>
2024-10-29 23:03:04 +0100

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import StrømApp from './components/StrømApp/StrømApp';
+import Strømavtalen from './components/Strømavtalen/Strømavtalen'
 
 function App() {
   return (
     <div>
       <Header />
       <StrømApp />
+      <Strømavtalen />
       
     </div>
   );
---
+++ src/components/Strømavtalen/Strømavtalen.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import styles from './Strømavtalen.module.css';
+
+const Strømavtalen = () => {
+  return (
+    <section id="strømavtalen" className={`${styles.section} ${styles.lessonSection}`}>
+      <div className={styles.container}>
+        <h2 className={styles.sectionTitle}>
+          Bestill <span>strømavtalen</span>
+        </h2>
+        <p className={styles.sectionSubtitle}>
+          Nord-Salten Kraft AS er kjent for å være en seriøs, solid, stabil og
+          konkurransedyktig leverandør av fornybar energi.
+        </p>
+        <ul className={styles.lessonsList}>
+          <li className={styles.lessonItem}>
+            <h3 className={styles.lessonItemTitle}>Lokalkraft</h3>
+            <h4 className={styles.lessonItemPrice}>
+              Prisen beregnes etterskuddsvis hver måned basert på gjennomsnittlig
+              spotpris
+            </h4>
+            <p className={styles.lessonItemText}>
+              Velger du Lokalkraft slipper du å ta stilling til om du bør binde
+              strømprisen framover eller om du skal ta risikoen med å ligge på
+              flytende markedspris (spot).
+            </p>
+            <p className={styles.lessonItemText}>eFaktura 0 kr / papirfaktura 9,75 kr</p>
+            <a href="#application-form" className={`${styles.button} ${styles.lessonButton}`}>Bestill nå</a>
+          </li>
+          <li className={`${styles.lessonItem} ${styles.activeItem}`}>
+            <h3 className={styles.lessonItemTitle}>Spotpris</h3>
+            <h4 className={styles.lessonItemPrice}>Spotpris Nordpool +3,99 øre/kWh</h4>
+            <p className={styles.lessonItemText}>
+              Spot er avtalen for deg som ønsker å følge strømprisen i
+              kraftmarkedet. Velger du Spot har du alltid en markedsriktig pris.<br />
+              → Ingen binding.<br />
+              → Ingen månedsgebyr.
+            </p>
+            <p className={styles.lessonItemText}>eFaktura 0 kr / papirfaktura 9,75 kr</p>
+            <p className={styles.lessonItemText}>
+              Avtalen har ingen bindingstid og faktureres etterskuddsvis månedlig
+              etter faktisk forbruk.
+            </p>
+            <a href="#application-form" className={`${styles.button} ${styles.lessonButton} ${styles.active}`}>Bestill nå</a>
+          </li>
+          <li className={styles.lessonItem}>
+            <h3 className={styles.lessonItemTitle}>Fastpris</h3>
+            <p className={styles.lessonItemText}>
+              Med fastpris er du garantert den samme prisen per kWh i den avtalte
+              perioden. Vi tilbyr fastprisavtaler for 1 år og 3 år. Uansett
+              markedsutvikling, vil du med en fastprisavtale alltid vite hvilken
+              kraftpris du betaler.
+            </p>
+            <p className={styles.lessonItemText}>eFaktura 0 kr / papirfaktura 9,75 kr</p>
+            <p className={styles.lessonItemText}>
+              Fastprisavtaler kan ikke sies opp eller endres i avtaleperioden uten
+              at det vil påløpe et bruddgebyr.
+            </p>
+            <a href="#application-form" className={`${styles.button} ${styles.lessonButton}`}>Bestill nå</a>
+          </li>
+        </ul>
+        <p className={styles.sectionSubtitle}>
+          Strømavtalen selges kun til strømkunder i Kystnett sitt nettområde, dvs
+          Hamarøy, Steigen, Kjøpsvik og nordre Sørfold.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Strømavtalen;
