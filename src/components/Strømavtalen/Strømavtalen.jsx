e43c5c4a3432594f95b5379038fbaa90d812447b
tobolovskaya <142045878+tobolovskaya@users.noreply.github.com>
2024-10-30 21:48:28 +0100

    1

--- src/components/Strømavtalen/Strømavtalen.jsx
+++ src/components/Strømavtalen/Strømavtalen.jsx
@@ -15,48 +15,48 @@ const Strømavtalen = () => {
         <ul className={styles.sectionList}>
           <li className={styles.sectionItem}>
             <h3 className={styles.sectionItemTitle}>Lokalkraft</h3>
-            <h4 className={styles.lessonItemPrice}>
+            <h4 className={styles.sectionItemPrice}>
               Prisen beregnes etterskuddsvis hver måned basert på gjennomsnittlig
               spotpris
             </h4>
-            <p className={styles.lessonItemText}>
+            <p className={styles.sectionItemText}>
               Velger du Lokalkraft slipper du å ta stilling til om du bør binde
               strømprisen framover eller om du skal ta risikoen med å ligge på
               flytende markedspris (spot).
             </p>
-            <p className={styles.lessonItemText}>eFaktura 0 kr / papirfaktura 9,75 kr</p>
-            <a href="#application-form" className={`${styles.button} ${styles.lessonButton}`}>Bestill nå</a>
+            <p className={styles.sectionItemText}>eFaktura 0 kr / papirfaktura 9,75 kr</p>
+            <a href="#application-form" className={`${styles.button} ${styles.sectionButton}`}>Bestill nå</a>
           </li>
           <li className={`${styles.sectionItem} ${styles.activeItem}`}>
             <h3 className={styles.sectionItemTitle}>Spotpris</h3>
-            <h4 className={styles.lessonItemPrice}>Spotpris Nordpool +3,99 øre/kWh</h4>
-            <p className={styles.lessonItemText}>
+            <h4 className={styles.sectionItemPrice}>Spotpris Nordpool +3,99 øre/kWh</h4>
+            <p className={styles.sectionItemText}>
               Spot er avtalen for deg som ønsker å følge strømprisen i
               kraftmarkedet. Velger du Spot har du alltid en markedsriktig pris.<br />
               → Ingen binding.<br />
               → Ingen månedsgebyr.
             </p>
-            <p className={styles.lessonItemText}>eFaktura 0 kr / papirfaktura 9,75 kr</p>
-            <p className={styles.lessonItemText}>
+            <p className={styles.sectionItemText}>eFaktura 0 kr / papirfaktura 9,75 kr</p>
+            <p className={styles.sectionItemText}>
               Avtalen har ingen bindingstid og faktureres etterskuddsvis månedlig
               etter faktisk forbruk.
             </p>
-            <a href="#application-form" className={`${styles.button} ${styles.lessonButton} ${styles.active}`}>Bestill nå</a>
+            <a href="#application-form" className={`${styles.button} ${styles.sectionButton} ${styles.active}`}>Bestill nå</a>
           </li>
           <li className={styles.sectionItem}>
             <h3 className={styles.sectionItemTitle}>Fastpris</h3>
-            <p className={styles.lessonItemText}>
+            <p className={styles.sectionItemText}>
               Med fastpris er du garantert den samme prisen per kWh i den avtalte
               perioden. Vi tilbyr fastprisavtaler for 1 år og 3 år. Uansett
               markedsutvikling, vil du med en fastprisavtale alltid vite hvilken
               kraftpris du betaler.
             </p>
-            <p className={styles.lessonItemText}>eFaktura 0 kr / papirfaktura 9,75 kr</p>
-            <p className={styles.lessonItemText}>
+            <p className={styles.sectionItemText}>eFaktura 0 kr / papirfaktura 9,75 kr</p>
+            <p className={styles.sectionItemText}>
               Fastprisavtaler kan ikke sies opp eller endres i avtaleperioden uten
               at det vil påløpe et bruddgebyr.
             </p>
-            <a href="#application-form" className={`${styles.button} ${styles.lessonButton}`}>Bestill nå</a>
+            <a href="#application-form" className={`${styles.button} ${styles.sectionButton}`}>Bestill nå</a>
           </li>
         </ul>
         <p className={styles.sectionSubtitle}>
